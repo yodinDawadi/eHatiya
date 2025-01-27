886fc38b6999faad8115f0475771e12cfe58c1a7
yodinDawadi <yodindawadi10@gmail.com>
2025-01-27 23:49:11 +0545

    cart icon update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,8 +2,11 @@ import { Button } from "@mui/material";
 import React from "react";
 import { BrowserRouter, NavLink, Router, Routes } from "react-router-dom";
 import Searchbar from "./Searchbar";
+import { LuShoppingCart } from "react-icons/lu";
+import { useCart } from "../Context/cartContext";
 
 const Navbar = () => {
+    const {cartProduct} = useCart();
   return (
     <nav className="flex items-center justify-center h-20 px-2">
       <div id="text" className="flex w-1/3">
@@ -11,7 +14,7 @@ const Navbar = () => {
       </div>
       <div className="w-1/3 flex gap-5 items-center">
         <Searchbar />
-        <NavLink to={"/cart"}>cart</NavLink>
+        <NavLink className={'flex'} to={"/cart"}><LuShoppingCart/><sup className="flex items-center justify-center text-white bg-red-700 rounded-full w-4 h-4">{cartProduct.length}</sup></NavLink>
       </div>
 
       <div className="w-1/3 flex items-center justify-between gap-5" id="links">
