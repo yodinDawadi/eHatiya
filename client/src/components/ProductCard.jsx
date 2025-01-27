2aa499dd0c7c7a97ee2553a58afd5e90ecbb196a
SushantPokhrel <sushant.805421@cct.tu.edu.np>
2025-01-27 21:03:34 +0545

    fakestore api

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import farsi from '../assets/farsi.jpg'
-import { Button } from '@mui/material'
+import React from "react";
+import farsi from "../assets/farsi.jpg";
+import { Button } from "@mui/material";
 
-const ProductCard = () => {
+const ProductCard = ({ item }) => {
   return (
-
-    <div className='w-1/4 h-1/2 flex flex-col rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
-        <img className='rounded-t-lg' src={farsi} alt="sabji image" />
-        <div className='w-full p-10 flex flex-col gap-2'>
-            <p className='text-4xl capitalize'>Title</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente magnam mollitia laboriosam! Veritatis, sapiente. Mollitia assumenda fuga voluptatum error maxime aspernatur reiciendis facilis alias quasi ab, dolores numquam molestiae. Eos laborum corrupti iusto culpa recusandae voluptates perspiciatis amet itaque, ullam quam? Dolor dolorem exercitationem deserunt distinctio adipisci laudantium atque libero.</p>
-        <Button variant='contained' color='success'>Add to cart</Button>
-        </div>
+    <div className="w-1/4 h-1/2 flex flex-col rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+      <img className="rounded-t-lg" src={item.image} alt="sabji image" />
+      <div className="w-full p-10 flex flex-col gap-2">
+        <p className="text-4xl capitalize">{item.title}</p>
+        <p className="text-xl">{item.description}</p>
+        <Button variant="contained" color="success">
+          Add to cart
+        </Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
--- client/src/components/shared/Products.jsx
+++ client/src/components/shared/Products.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import ProductCard from '../ProductCard'
+import React from "react";
+import ProductCard from "../ProductCard";
 
 const Products = () => {
+  const [products, setProducts] = React.useState([]);
+  React.useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setProducts(data);
+      });
+  }, []);
   return (
-<>
-<div className='flex flex-wrap gap-20 justify-center items-center '>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-    </div>
-</>
-    
-  )
-}
+    <>
+      <div className="flex flex-wrap gap-20 justify-center items-center ">
+        {products.map((item,key) => (
+          <ProductCard item={item} key={key} />
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default Products
+export default Products;
